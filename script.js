f2449d58cf5f97952f2568c3030cb56c37b554ae
mauryyaamit <mauryaamit2062@gmail.com>
2025-08-08 17:05:58 +0530

    Made loader successfully

---
+++ script.js
@@ -0,0 +1,43 @@
+
+var tl = gsap.timeline()
+tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay:0.5
+})
+tl.from("#line1-part1",{
+    opacity: 0,
+    onStart: function () {
+        var count = 0;
+var h5timer=document.querySelector(".line #line1-part1 h5");
+setInterval(function () {
+    if (count <100) {
+        h5timer.innerHTML = count++;
+    }
+    else {
+        h5timer.innerHTML = count;
+    }
+}, 33)
+    }
+})
+    
+tl.to(".line h2", {
+    opacity: 1,
+    animationName:"anime"
+})
+tl.to("#loader", {
+    opacity: 0,
+    duration: 0.2,
+    delay:4
+})
+tl.from("#page1", {
+    delay:0.2,
+    y: 1600,
+    opacity: 0,
+    duration:0.5,
+    ease:Power4
+})
+tl.to("#loader", {
+    display: "none"
+})
